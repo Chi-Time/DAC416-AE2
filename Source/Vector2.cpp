eb475b8ec39094d3acdf745b9a47cf9ba27878df
James Johnson <music1384@yahoo.co.uk>
2018-01-15 11:39:22 +0000

    Refactor class

--- Source/Vector2.cpp
+++ Source/Vector2.cpp
@@ -1,12 +1,1 @@
 #include "Vector2.h"
-
-
-
-Vector2::Vector2 ()
-{
-}
-
-
-Vector2::~Vector2 ()
-{
-}
--- Source/Vector2.h
+++ Source/Vector2.h
@@ -3,28 +3,19 @@
 
 #include "cVector2.h"
 
-class Vector2 : cVector2
+/* Description: Extension helper class for base cVector2*/
+class Vector2
 {
 // Getters/Setters
 public:
-	/* Description: Shorthand for returning a cVector2 of type: {-1, 0}
-	*/
+	/* Description: Shorthand for returning a cVector2 of type: {-1, 0}*/
 	static cVector2 Left () { return cVector2 (-1, 0); }
-	/* Description: Shorthand for returning a cVector2 of type: {1, 0}
-	*/
+	/* Description: Shorthand for returning a cVector2 of type: {1, 0}*/
 	static cVector2 Right () { return cVector2 (1, 0); }
-	/* Description: Shorthand for returning a cVector2 of type: {0, -1}
-	*/
+	/* Description: Shorthand for returning a cVector2 of type: {0, -1}*/
 	static cVector2 Up () { return cVector2 (0, -1); }
-	/* Description: Shorthand for returning a cVector2 of type: {0, 1}
-	*/
+	/* Description: Shorthand for returning a cVector2 of type: {0, 1}*/
 	static cVector2 Down () { return cVector2 (0, 1); }
-
-// Methods
-public:
-	Vector2 ();
-	~Vector2 ();
-
 };
 
 #endif // !INCLUDE_H_VECTOR2
